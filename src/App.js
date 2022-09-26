282c723aedf081ed2024e13de74902a4aeacb84b
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-26 16:09:59 +0300

    add browser router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Rockets from './Pages/Rockets';
+import Missions from './Pages/Missions';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/mission" element={<Missions />} />
+        <Route path="/my-profile" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
 

277136bd7039ffaad5a5f4d6ed602131a4821ef6
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-27 16:03:34 +0300

    add rocketsRedux after renaming

--- src/Redux/configureStore.jsx
+++ src/Redux/configureStore.jsx
@@ -1,6 +1,6 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import { rocketReducer } from './Rocket/Rockets';
+import { rocketReducer } from './Rocket/RocketsRedux';
 
 const rootReducer = combineReducers({
   rocketReducer,

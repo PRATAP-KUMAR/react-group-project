eb7bd6927c8ad4d094468efeb71d8ed320b87e07
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-27 16:02:10 +0300

    add rockets page after renaming

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Rockets from './Pages/Rockets';
 import Missions from './Pages/Missions';
 import Profile from './Pages/Profile';
+import RocketsPage from './Pages/RocketsPage';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Rockets />} />
+        <Route path="/" element={<RocketsPage />} />
         <Route path="/mission" element={<Missions />} />
         <Route path="/my-profile" element={<Profile />} />
       </Routes>

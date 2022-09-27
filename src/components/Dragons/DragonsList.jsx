dcad13543999c88e560cabf4a31708d593c25ac1
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-27 15:42:29 +0100

    [feature-dragon-APIfetch]: Added side Effect to dispatch FetchDragons call

--- src/components/Dragons/DragonsList.jsx
+++ src/components/Dragons/DragonsList.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  getStatus,
+  fetchDragons,
+} from '../../Redux/Dragons/dragonsSlice';
 
 const DragonsList = () => {
-  return <div>DragonsList</div>;
+  const dispatch = useDispatch();
+  const status = useSelector(getStatus);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchDragons());
+    }
+  }, [status, dispatch]);
+
+  return <div>DragonList</div>;
 };
 
 export default DragonsList;

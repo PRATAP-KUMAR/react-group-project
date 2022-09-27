f22944a03bb575f980367b608408e44c92467167
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-27 14:18:14 +0100

    [feature-dragon-APIfetch]: Update : Change the StrictMode Policy

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );

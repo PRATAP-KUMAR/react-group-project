b32f3a9ce391530831b98da525dabf6bf5839e12
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-27 13:18:51 +0100

    [feature-dragon-Structure]: Add: Dragons UI page

---
+++ src/Pages/Dragons/Dragons.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DragonsList from '../../components/Dragons/DragonsList';
+
+const Dragons = () => (
+  <div>
+    <h2>Welcome to Dragons</h2>
+  </div>
+);
+
+export default Dragons;

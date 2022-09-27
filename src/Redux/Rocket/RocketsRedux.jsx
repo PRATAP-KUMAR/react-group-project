3e82b158a8665a3df960da8105ee69630eb1792f
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-27 20:18:10 +0300

    add action creator and handler when reserving a rocket

--- src/Redux/Rocket/RocketsRedux.jsx
+++ src/Redux/Rocket/RocketsRedux.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API_URL = 'https://api.spacexdata.com/v3/rockets';
 const GET_ROCKET = 'GET_ROCKET';
+const RESERVE_ROCKET = 'RESERVE_ROCKET';
 
 const fetchRockets = () => (dispatch) => {
   axios.get(API_URL).then((response) => {
@@ -10,18 +11,31 @@ const fetchRockets = () => (dispatch) => {
   });
 };
 
+const bookRocket = (id) => (dispatch) => {
+  dispatch({ type: RESERVE_ROCKET, id });
+};
+
 const initialState = [];
 
 const rocketReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ROCKET:
-      console.log(...action.rockets);
       return [
         ...action.rockets,
+      ];
+    case RESERVE_ROCKET:
+      return [
+        ...state.map((rocket) => {
+          if (rocket.rocket_id !== action.id) {
+            // console.log(rocket.rocket_id, action.id);
+            return rocket;
+          }
+          return { ...rocket, reserved: true };
+        }),
       ];
     default: return state;
   }
 };
 
 export default fetchRockets;
-export { rocketReducer };
+export { rocketReducer, bookRocket };

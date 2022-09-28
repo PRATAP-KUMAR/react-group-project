1d592e81285e7a4788c325a506915d2c57e53192
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-28 08:51:33 +0100

    [feature-dragon-render]: Import component for Display

---
+++ src/Pages/Dragons/DragonsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DragonsList from '../../components/Dragons/DragonsList';
+
+const DragonsPage = () => (
+  <div>
+    <DragonsList />
+  </div>
+);
+
+export default DragonsPage;

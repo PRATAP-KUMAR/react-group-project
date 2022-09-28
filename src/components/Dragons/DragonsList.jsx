7ad297cbd8aefc21b1f25f4c920cc61f074ba4bd
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-28 08:50:55 +0100

    [feature-dragon-render]: add: useSelectors and conditional for displaying Dragons depending on status

--- src/components/Dragons/DragonsList.jsx
+++ src/components/Dragons/DragonsList.jsx
@@ -1,13 +1,20 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
+  getDragons,
   getStatus,
+  getError,
   fetchDragons,
+
 } from '../../Redux/Dragons/dragonsSlice';
+import Dragon from './Dragon';
+
 
 const DragonsList = () => {
   const dispatch = useDispatch();
+  const dragons = useSelector(getDragons);
   const status = useSelector(getStatus);
+  const error = useSelector(getError);
 
   useEffect(() => {
     if (status === 'idle') {
@@ -15,6 +22,22 @@ const DragonsList = () => {
     }
   }, [status, dispatch]);
 
-  return <div>DragonList</div>;
+let content;
+
+  if (status === 'Loading') {
+    content = <p>&quot,loading...&quot,</p>;
+  } else if (status === 'succeeded') {
+    content = (
+      <ul>
+        {dragons.map((dragon) => (
+          <Dragon key={dragon.id} dragon={dragon} />
+        ))}
+      </ul>
+    );
+  } else if (status === 'failed') {
+    content = <p>{error}</p>;
+  }
+
+  return <div>{content}</div>;
 };
 export default DragonsList;

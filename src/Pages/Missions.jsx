88c3e22690433fb02c0e049a9fa5b87d7717c952
PRATAP-KUMAR <pratap@fastmail.fm>
2022-09-28 14:25:45 +0530

    MISSION JOINING, LEAVING, SWITCH BADGES

--- src/Pages/Missions.jsx
+++ src/Pages/Missions.jsx
@@ -1,20 +1,30 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getMissionsFromAPIAction } from '../Redux/Missions/missions';
+import { getMissionsFromAPIAction, joinMissionAction, leaveMissionAction } from '../Redux/Missions/missions';
 import '../components/css/Missions.css';
 
 const Missions = () => {
   const missionsData = useSelector((state) => state.MissionsDataReducer);
+  console.log(missionsData);
   const dispatch = useDispatch();
+
+  const handleButtonClickAction = (e) => {
+    e.preventDefault();
+    const { id } = e.target;
+    if (e.target.textContent === 'Join Mission') dispatch(joinMissionAction(id));
+    if (e.target.textContent === 'Leave Mission') dispatch(leaveMissionAction(id));
+  };
 
   useEffect(() => {
     if (missionsData.length === 0) {
       dispatch(getMissionsFromAPIAction());
+      // console.log(missionsData[0]);
     }
   }, [dispatch, missionsData.length]);
 
   const itemList = missionsData.map((mission) => (
     <>
+      {console.log(mission.reserved)}
       <div className="missions-data">
         <div className="col-1">
           {mission.mission_name}
@@ -22,11 +32,17 @@ const Missions = () => {
         <div className="col-2">
           {mission.description}
         </div>
-        <div className="col-3">
-          status
+        <div className={mission.reserved === true ? 'col-3 reserved' : 'col-3 unreserved'}>
+          {mission.reserved === true ? 'Active Member' : 'Not a Member'}
         </div>
-        <div className="col-4">
-          Join/Leave
+        <div className={mission.reserved === true ? 'col-4 join-mission' : 'col-4 leave-mission'}>
+          <button
+            type="submit"
+            id={mission.mission_id}
+            onClick={handleButtonClickAction}
+          >
+            {mission.reserved === true ? 'Leave Mission' : 'Join Mission'}
+          </button>
         </div>
       </div>
     </>

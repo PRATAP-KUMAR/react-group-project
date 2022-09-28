134041033c14064ff4570e8f2230b0f8773833a4
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-28 16:56:03 +0100

    [feature-dragon-profile]:  update: Change profile to Accept components with props

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,10 +1,17 @@
-import DragonProfile from "../components/Dragons/DragonProfile";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { getDragons } from '../Redux/Dragons/dragonsSlice';
+import SpaceProfile from '../components/Dragons/Profiles';
 
-const Profile = () => (
-  <div className="profile__container">
-    <DragonProfile/>
-  </div>
-
-);
+const Profile = () => {
+  const dragons = useSelector(getDragons);
+  console.log(dragons);
+  return (
+    <div className="profile__container">
+      <SpaceProfile items={dragons} header={'My Dragons'} />
+      <SpaceProfile items={dragons} header={'My Rockets'} />
+    </div>
+  );
+};
 
 export default Profile;

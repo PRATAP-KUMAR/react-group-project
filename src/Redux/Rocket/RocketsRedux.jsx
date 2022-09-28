d6905d1a81e7a3c4bc7cfbc0debd5275f2771070
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-28 15:05:20 +0300

    add action creator and handler for cancel reservation

--- src/Redux/Rocket/RocketsRedux.jsx
+++ src/Redux/Rocket/RocketsRedux.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 const API_URL = 'https://api.spacexdata.com/v3/rockets';
 const GET_ROCKET = 'GET_ROCKET';
 const RESERVE_ROCKET = 'RESERVE_ROCKET';
+const CANCEL_RESERVATION = 'CANCEL_RESERVATION';
 
 const fetchRockets = () => (dispatch) => {
   axios.get(API_URL).then((response) => {
@@ -13,6 +14,10 @@ const fetchRockets = () => (dispatch) => {
 
 const bookRocket = (id) => (dispatch) => {
   dispatch({ type: RESERVE_ROCKET, id });
+};
+
+const cancelReservation = (id) => (dispatch) => {
+  dispatch({ type: CANCEL_RESERVATION, id });
 };
 
 const initialState = [];
@@ -33,9 +38,19 @@ const rocketReducer = (state = initialState, action) => {
           return { ...rocket, reserved: true };
         }),
       ];
+    case CANCEL_RESERVATION:
+      return [
+        ...state.map((rocket) => {
+          if (rocket.rocket_id !== action.id) {
+            // console.log(rocket.rocket_id, action.id);
+            return rocket;
+          }
+          return { ...rocket, reserved: false };
+        }),
+      ];
     default: return state;
   }
 };
 
 export default fetchRockets;
-export { rocketReducer, bookRocket };
+export { rocketReducer, bookRocket, cancelReservation };
